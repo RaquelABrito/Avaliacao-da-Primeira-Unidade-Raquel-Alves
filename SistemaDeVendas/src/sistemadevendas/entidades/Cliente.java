dc3cfaa4faed4b70efb086f6d7c7b40143ad8965
Raquel Alves <51912497+RaquelABrito@users.noreply.github.com>
2021-03-21 16:54:38 -0300

    update

--- SistemaDeVendas/src/sistemadevendas/entidades/Cliente.java
+++ SistemaDeVendas/src/sistemadevendas/entidades/Cliente.java
@@ -10,12 +10,67 @@ package sistemadevendas.entidades;
  * @author Concursos
  */
 public class Cliente {
+
+    /**
+     * @return the cfp
+     */
+    public String getCfp() {
+        return cfp;
+    }
+
+    /**
+     * @param cfp the cfp to set
+     */
+    public void setCfp(String cfp) {
+        this.cfp = cfp;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public float getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the valoresPagar
+     */
+    public float getValoresPagar() {
+        return valoresPagar;
+    }
+
+    /**
+     * @param valoresPagar the valoresPagar to set
+     */
+    public void setValoresPagar(float valoresPagar) {
+        this.valoresPagar = valoresPagar;
+    }
     private String cfp;
     private String nome;
     private float saldo;
     private float valoresPagar;
    // private String comprasRealizadas;
-    
     
     
 }
